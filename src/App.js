2964cb4282134de15c730a4178efacddedd78787
Rajat Mishra <rajat.m324@gmail.com>
2022-01-30 18:20:32 +0530

    added json-server library to understand and learn "fetch" library to make API calls.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,81 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import { useState } from "react";
 import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors appointment",
-      day: "Feb 5th at 2:30 pm",
-      reminder: false,
-    },
-    {
-      id: 2,
-      text: "Headphone repair",
-      day: "Feb 6th at 2:30 pm",
-      reminder: false,
-    },
-    {
-      id: 3,
-      text: "New Task",
-      day: "Feb 7th at 2:30 pm",
-      reminder: true,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
   const [showAddTask, setShowAddTask] = useState(false);
 
+  useEffect(() => {
+    const getTasks = async () => {
+      const taskFromServer = await fetchTasks();
+      setTasks(taskFromServer);
+    };
+    getTasks();
+  }, []);
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const response = await fetch("http://localhost:5000/tasks");
+    const data = await response.json();
+    return data;
+  };
+
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const response = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await response.json();
+    return data;
+  };
+
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
-    console.log("Add task called with id", id);
+  const addTask = async (task) => {
+    try {
+      const response = await fetch(`http://localhost:5000/tasks`, {
+        method: "POST",
+        headers: {
+          "Access-Control-Allow-Headers": "Content-Type",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(task),
+      });
+      const newTask = await response.json();
+      setTasks([...tasks, newTask]);
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
   };
 
   // Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    });
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+    const response = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    });
+
+    const data = await response.json();
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
@@ -53,15 +85,25 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <Header onAdd={add} showAddTask={showAddTask} />
-      {showAddTask && <AddTask onAdd="addTask" onAdd={addTask} />}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-        "No tasks to show"
-      )}
-    </div>
+    <Router>
+      <div className="container">
+        <Header onAdd={add} showAddTask={showAddTask} />
+        {showAddTask && <AddTask onAdd="addTask" onAdd={addTask} />}
+        {tasks.length > 0 ? (
+          <Tasks
+            tasks={tasks}
+            onDelete={deleteTask}
+            onToggle={toggleReminder}
+          />
+        ) : (
+          "No tasks to show"
+        )}
+        <Routes>
+          <Route path="/about" component={About} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
